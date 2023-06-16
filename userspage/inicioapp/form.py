52132a3866b3d19316694f0a5153a10217410542
Guido Alejandro Saavedra Torres <galejandrosaavedra@gmail.com>
2023-06-16 10:52:11 -0400

    commit con programa completo

---
+++ userspage/inicioapp/form.py
@@ -0,0 +1,6 @@
+# form para registrar los errores de los campos al crear pass
+# Su contraseña no puede asemejarse tanto a su otra información personal.
+# Su contraseña debe contener al menos 8 caracteres.
+# Su contraseña no puede ser una clave utilizada comúnmente.
+# Su contraseña no puede ser completamente numérica.
+# con django
